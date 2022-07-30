f3cb4fb7ffbe5be175074dff5cdd18a0b2f02496
Pablo Dias <pabllosoarez@gmail.com>
2022-07-30 17:11:02 -0300

    pre-add dropdown menu

---
+++ script.js
@@ -0,0 +1,22 @@
+// const dropdown = document.getElementsByClassName("dropdown")
+// const discoverDrop = document.querySelector(".discover-drop")
+// const categoriesDrop = document.querySelector(".categories-drop") 
+
+// const discoverMenu = document.querySelector(".categories")
+// const categoriesMenu = document.querySelector(".categories")
+
+// const overflow = document.getElementById("overflow")
+
+
+// discoverMenu.addEventListener("mouseenter", () => {
+//     discoverDrop.classList.remove("hide")
+//     overflow.classList.remove("hide");
+// })
+
+// discoverDrop.addEventListener("mouseleave", () => {
+//     discoverDrop.classList.add("hide")
+//     overflow.classList.add("hide");
+// })
+
+
+
